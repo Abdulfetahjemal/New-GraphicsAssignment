92cb7f9ed1b619710686e9ec784c2b25df6b7e72
Abdulfetahjemal <32792371+Abdulfetahjemal@users.noreply.github.com>
2020-10-08 16:44:30 +0300

    3d setup

--- GraphicsAssignment/Source.cpp
+++ GraphicsAssignment/Source.cpp
@@ -1,44 +1,42 @@
+#include<stdio.h>
+#include<iostream>
+#include<GL/glut.h>
 
-/* OGL02Animation.cpp: 3D Shapes with animation
-*/
-//#include <windows.h>  // for MS Windows
-#include <GL/glut.h>  // GLUT, include glu.h and gl.h
+#define KEY_ESC 27  
 
-/* Global variables */
-char title[] = "3D Shapes with animation";
-GLfloat anglePyramid = 0.0f;  // Rotational angle for pyramid [NEW]
-GLfloat angleCube = 0.0f;     // Rotational angle for cube [NEW]
-int refreshMills = 15;        // refresh interval in milliseconds [NEW]
+using namespace std;
 
-/* Initialize OpenGL Graphics */
-void initGL() {
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
-	glClearDepth(1.0f);                   // Set background depth to farthest
-	glEnable(GL_DEPTH_TEST);   // Enable depth testing for z-culling
-	glDepthFunc(GL_LEQUAL);    // Set the type of depth-test
-	glShadeModel(GL_SMOOTH);   // Enable smooth shading
-	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  // Nice perspective corrections
-}
+int fullscreen = 0;
+int mouseDown = 0;
 
-/* Handler for window-repaint event. Called back when the window first appears and
-   whenever the window needs to be re-painted. */
-void display() {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
-	glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
+float xrot = 100.0f;
+float yrot = -100.0f;
 
-	// Render a color-cube consisting of 6 quads with different colors
-	glLoadIdentity();                 // Reset the model-view matrix
-	glTranslatef(1.5f, 0.0f, -7.0f);  // Move right and into the screen
-	glRotatef(angleCube, 1.0f, 1.0f, 1.0f);  // Rotate about (1,1,1)-axis [NEW]
+float xdiff = 100.0f;
+float ydiff = 100.0f;
 
+float tra_x = 0.0f;
+float tra_y = 0.0f;
+float tra_z = 0.0f;
+
+
+float grow_shrink = 70.0f;
+float resize_f = 1.0f;
+
+
+
+
+void drawBox()
+{
+
+
+	glTranslatef(tra_x, tra_y, tra_z);
 	glBegin(GL_QUADS);                // Begin drawing the color cube with 6 quads
-	   // Top face (y = 1.0f)
-	   // Define vertices in counter-clockwise (CCW) order with normal pointing out
-	glColor3f(0.0f, 1.0f, 0.0f);     // Green
-	glVertex3f(1.0f, 1.0f, -1.0f);
-	glVertex3f(-1.0f, 1.0f, -1.0f);
-	glVertex3f(-1.0f, 1.0f, 1.0f);
-	glVertex3f(1.0f, 1.0f, 1.0f);
+		// Top face (y = 1.0f)
+		// Define vertices in counter-clockwise (CCW) order with normal pointing out
+	// Green 
+
+
 
 	// Bottom face (y = -1.0f)
 	glColor3f(1.0f, 0.5f, 0.0f);     // Orange
@@ -76,86 +74,207 @@ void display() {
 	glVertex3f(1.0f, -1.0f, -1.0f);
 	glEnd();  // End of drawing color-cube
 
-	// Render a pyramid consists of 4 triangles
-	glLoadIdentity();                  // Reset the model-view matrix
-	glTranslatef(-1.5f, 0.0f, -6.0f);  // Move left and into the screen
-	glRotatef(anglePyramid, 1.0f, 1.0f, 0.0f);  // Rotate about the (1,1,0)-axis [NEW]
 
-	glBegin(GL_TRIANGLES);           // Begin drawing the pyramid with 4 triangles
-	   // Front
-	glColor3f(1.0f, 0.0f, 0.0f);     // Red
-	glVertex3f(0.0f, 1.0f, 0.0f);
-	glColor3f(0.0f, 1.0f, 0.0f);     // Green
-	glVertex3f(-1.0f, -1.0f, 1.0f);
-	glColor3f(0.0f, 0.0f, 1.0f);     // Blue
-	glVertex3f(1.0f, -1.0f, 1.0f);
 
-	// Right
-	glColor3f(1.0f, 0.0f, 0.0f);     // Red
-	glVertex3f(0.0f, 1.0f, 0.0f);
-	glColor3f(0.0f, 0.0f, 1.0f);     // Blue
-	glVertex3f(1.0f, -1.0f, 1.0f);
-	glColor3f(0.0f, 1.0f, 0.0f);     // Green
-	glVertex3f(1.0f, -1.0f, -1.0f);
-
-	// Back
-	glColor3f(1.0f, 0.0f, 0.0f);     // Red
-	glVertex3f(0.0f, 1.0f, 0.0f);
-	glColor3f(0.0f, 1.0f, 0.0f);     // Green
-	glVertex3f(1.0f, -1.0f, -1.0f);
-	glColor3f(0.0f, 0.0f, 1.0f);     // Blue
-	glVertex3f(-1.0f, -1.0f, -1.0f);
-
-	// Left
-	glColor3f(1.0f, 0.0f, 0.0f);       // Red
-	glVertex3f(0.0f, 1.0f, 0.0f);
-	glColor3f(0.0f, 0.0f, 1.0f);       // Blue
-	glVertex3f(-1.0f, -1.0f, -1.0f);
-	glColor3f(0.0f, 1.0f, 0.0f);       // Green
-	glVertex3f(-1.0f, -1.0f, 1.0f);
-	glEnd();   // Done drawing the pyramid
-
-	glutSwapBuffers();  // Swap the front and back frame buffers (double buffering)
-
-	// Update the rotational angle after each refresh [NEW]
-	anglePyramid += 0.2f;
-	angleCube -= 0.15f;
 }
 
-/* Called back when timer expired [NEW] */
-void timer(int value) {
-	glutPostRedisplay();      // Post re-paint request to activate display()
-	glutTimerFunc(refreshMills, timer, 0); // next timer call milliseconds later
+int init(void)
+{
+	glClearColor(0.93f, 0.93f, 0.93f, 0.0f);
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glClearDepth(1.0f);
+
+	return 1;
 }
 
-/* Handler for window re-size event. Called back when the window first appears and
-   whenever the window is re-sized with its new width and height */
-void reshape(GLsizei width, GLsizei height) {  // GLsizei for non-negative integer
-   // Compute aspect ratio of the new window
-	if (height == 0) height = 1;                // To prevent divide by 0
-	GLfloat aspect = (GLfloat)width / (GLfloat)height;
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
 
-	// Set the viewport to cover the new window
-	glViewport(0, 0, width, height);
+	gluLookAt(
+		0.0f, 0.0f, 3.0f,
+		0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f);
 
-	// Set the aspect ratio of the clipping volume to match the viewport
-	glMatrixMode(GL_PROJECTION);  // To operate on the Projection matrix
-	glLoadIdentity();             // Reset
-	// Enable perspective projection with fovy, aspect, zNear and zFar
-	gluPerspective(45.0f, aspect, 0.1f, 100.0f);
+	glRotatef(xrot, 1.0f, 0.0f, 0.0f);
+	glRotatef(yrot, 0.0f, 1.0f, 0.0f);
+
+	drawBox();
+
+	glFlush();
+	glutSwapBuffers();
 }
 
-/* Main function: GLUT runs as a console application starting at main() */
-int main(int argc, char** argv) {
-	glutInit(&argc, argv);            // Initialize GLUT
-	glutInitDisplayMode(GLUT_DOUBLE); // Enable double buffered mode
-	glutInitWindowSize(640, 480);   // Set the window's initial width & height
-	glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
-	glutCreateWindow(title);          // Create window with the given title
-	glutDisplayFunc(display);       // Register callback handler for window re-paint event
-	glutReshapeFunc(reshape);       // Register callback handler for window re-size event
-	initGL();                       // Our own OpenGL initialization
-	glutTimerFunc(0, timer, 0);     // First timer call immediately [NEW]
-	glutMainLoop();                 // Enter the infinite event-processing loop
+void resize(int w, int h)
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	glViewport(0, 0, w, h);
+
+	gluPerspective(grow_shrink, resize_f * w / h, resize_f, 100 * resize_f);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void idle(void)
+{
+	if (!mouseDown)
+	{
+		xrot += 0.3f;
+		yrot += 0.4f;
+	}
+
+	glutPostRedisplay();
+}
+
+
+void mySpecialFunction(int key, int x, int y)
+{
+	cout << "U -----------> rotate clockwise\n";
+	cout << "W or w ------> Up\n";
+	cout << "S or s -----> Down\n";
+	cout << "D or d ------> Right\n";
+	cout << "A or a ------> Left\n";
+	cout << "Z or z ------> Shrink\n";
+	cout << "X or x ------> Grow\n";
+	cout << "Z or z ------> Shrink\n";
+	cout << "Escape Key ---> exit the program\n\n";
+
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 27:
+		exit(1);
+		break;
+
+
+
+	case 'w':
+	case 'W':
+		tra_x += 0.1f;
+		break;
+	case 's':
+	case 'S':
+		tra_x -= 0.1f;
+		break;
+	case 'a':
+	case 'A':
+		tra_z -= 0.1f;
+		break;
+	case 'd':
+	case 'D':
+		tra_z += 0.1f;
+		break;
+	case 'u':
+	case 'U':
+		xrot += 1.0f;
+		yrot += 1.0f;
+		xdiff += 1.0f;
+		ydiff += 1.0f;
+		break;
+
+	case 'y':
+	case 'Y':
+		xrot -= 1.0f;
+		yrot -= 1.0f;
+		xdiff += 1.0f;
+		ydiff += 1.0f;
+		break;
+
+	case 'h':
+	case 'H':
+		mySpecialFunction(key, x, y);
+		break;
+	case 'Z':
+	case 'z':
+		grow_shrink--;
+		resize(800 , 800);
+
+		break;
+	case 'X':
+	case 'x':
+		grow_shrink++;
+		resize(800 , 800);
+
+		break;
+
+	}
+
+
+	glutPostRedisplay();
+}
+
+void specialKeyboard(int key, int x, int y)
+{
+	if (key == GLUT_KEY_F1)
+	{
+		fullscreen = !fullscreen;
+
+		if (fullscreen)
+			glutFullScreen();
+		else
+		{
+			glutReshapeWindow(800 , 800);
+			glutPositionWindow(50, 50);
+		}
+	}
+}
+
+void mouse(int button, int state, int x, int y)
+{
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		mouseDown = 1;
+
+		xdiff = x - yrot;
+		ydiff = -y + xrot;
+	}
+	else
+		mouseDown = 0;
+}
+
+void mouseMotion(int x, int y)
+{
+	if (mouseDown)
+	{
+		yrot = x - xdiff;
+		xrot = y + ydiff;
+
+		glutPostRedisplay();
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+
+	glutInitWindowPosition(50, 50);
+	glutInitWindowSize(800 , 800);
+
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+
+	glutCreateWindow("Graphics Assignment - Abdulfetah Jemal Omer (A) - FI1692");
+
+	glutDisplayFunc(display);
+	glutKeyboardFunc(keyboard);
+	glutSpecialFunc(specialKeyboard);
+	glutMouseFunc(mouse);
+	glutMotionFunc(mouseMotion);
+	glutReshapeFunc(resize);
+	//glutIdleFunc(idle);
+
+	if (!init())
+		return 1;
+
+	glutMainLoop();
+
 	return 0;
 }
